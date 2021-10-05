55dea3fdcd4095c57dcd4f4a2c25a8048d2661a1
Leonardo Sanò <leonardo.sano@sourcesense.com>
2021-10-05 17:11:54 +0200

    prova 4 commit

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.ComponentScan;
 @SpringBootApplication
 @ComponentScan("com.example.demo.supremaziatotale")
 public class DemoApplication {
-    //commit per prova 3 commit
+    //commit per prova 4 commit
     public static void main(String[] args){ SpringApplication.run(DemoApplication.class, args); }
 
 }
